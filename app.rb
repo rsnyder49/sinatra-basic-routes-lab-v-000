3e37d8c944c6773983a139cf94872b06a29f2828
Robert Snyder <r.snyder49@gmail.com>
2018-11-05 21:01:27 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is Robby"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is San Jose"
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is Stairway to Heaven"
+  end 
+  
 end
